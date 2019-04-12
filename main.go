5b8a94cff1bbeab214d35f879f669e57cabd79b9
unknown <varvara@gabbarelli.it>
2019-04-12 14:52:30 +0000

    http and structure of the response

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+type Service struct {
+	h func(http.ResponseWriter, *http.Request) error
+}
+
+func (h Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h.h(w, r)
+	if err != nil {
+		log.Println("error:", err)
+	}
+}
+
+func GetMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.Handle("/", Service{GetResponse})
+	return mux
+}
+
+func GetResponse(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
 func main() {
 
 }
